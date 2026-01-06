139ba300a8ce7ae0058da671bc88d5e4958f7142
Bugra <alibugra.tiryaki@std.yeditepe.edu.tr>
2026-01-06 15:36:36 +0300

    Expose tasks in TaskViewModel
    mutable state list

--- app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
@@ -1,13 +1,24 @@
 package com.yeditepe.finalexam.viewmodel
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import com.yeditepe.finalexam.model.Task
 
 class TaskViewModel : ViewModel() {
 
     // TODO 1: Create a mutable state list of Task objects
     // Initially add at least 2 tasks
+    val tasks = mutableStateListOf(
+        Task(id = 1, title = "Complete Final Project", isCompleted = false),
+        Task(id = 2, title = "Review Code", isCompleted = true)
+    )
 
     fun toggleTask(taskId: Int) {
         // TODO 2: Update isCompleted for the given task
+        val task = tasks.find { it.id == taskId }
+        task?.let {
+            val index = tasks.indexOf(it)
+            tasks[index] = it.copy(isCompleted = !it.isCompleted)
+        }
     }
 }
